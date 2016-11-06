53d39391860180ee223e5dab08dbde6ffbb3cf48
Like Ma <milkpku@gmail.com>
2016-11-06 23:25:49 +0800

    finish halfpart of bending force

--- src/Dynamic_Hindge.cpp
+++ src/Dynamic_Hindge.cpp
@@ -19,8 +19,10 @@ namespace BalloonFEM
 		for (size_t i = 0; i < m_tetra->films.size(); i++)
 			count_hindge += m_tetra->films[i].hindges.size();
         SpVec dphi(count_hindge);
+        SpMat ddphi(count_hindge, count_hindge);
 
-        /* compute detrivation of hindge angle theta */
+        ///////////////////////////////////////////////////////////////////////
+        /* compute hindge angle theta and phi, dphi, ddphi */
         int offset = 0;
         for(MIter f = m_tetra->films.begin(); f != m_tetra->films.end(); f++)
         {
@@ -42,36 +44,30 @@ namespace BalloonFEM
 
                 /* energy is 2*sin(x/2)^2 */
                 dphi(offset) = glm::dot(e, glm::cross(n0, n1));
+                ddphi.coeffRef(offset, offset) = glm::dot(n0, n1);
                 offset ++;
             }
         }
 
-        SpVec bendforce = dphi.transpose() * state.hindgeAngleGradient();
-
-        for(size_t i = 0; i < f_sum.size(); i++)
-        {
-            f_sum[i] += Vec3(
-                    bendforce(3*i    ), 
-                    bendforce(3*i + 1), 
-                    bendforce(3*i + 2));
-        }
-
-        std::vector<T> coefficients;
-        coefficients.clear();
-		coefficients.reserve( 2 * 9 * count_hindge);
+        
+        //////////////////////////////////////////////////////////////////////
+        /* compute heissen matrix and theta gradient */
+        std::vector<T> theta_coeff;
+        theta_coeff.clear();
+		theta_coeff.reserve( 2 * 9 * count_hindge);
 
         /* compute detrivation of hindge angle theta */
-        offset = 0;
+        offset = 0;     /* since there may be more than one film obj */
         for(MIter f = m_tetra->films.begin(); f != m_tetra->films.end(); f++)
         {
             for(PIter p = f->peices.begin(); p != f->peices.end(); p++)
             {
-                iVec3 &id = p->v_id;
+                iVec3 &v_id = p->v_id;
                 /* edge dire , norm and Area */
                 Vec3 e[3] = { 
-                    pos[id[2]] - pos[id[1]],
-                    pos[id[0]] - pos[id[2]],
-                    pos[id[1]] - pos[id[0]],
+                    pos[v_id[2]] - pos[v_id[1]],
+                    pos[v_id[0]] - pos[v_id[2]],
+                    pos[v_id[1]] - pos[v_id[0]],
                 };
 
                 Vec3 norm = - glm::cross( e[0], e[1] );
@@ -79,10 +75,10 @@ namespace BalloonFEM
                 norm /= A2;
 
                 /* height of vertex */
-                double h[3] = { 
-                    A2 / glm::length(e[0]),
-                    A2 / glm::length(e[1]),
-                    A2 / glm::length(e[2]),
+                double h_inv[3] = { 
+                    glm::length(e[0]) / A2,
+                    glm::length(e[1]) / A2,
+                    glm::length(e[2]) / A2,
                 };
 
                 /* cosine of vertex */
@@ -102,30 +98,37 @@ namespace BalloonFEM
                     glm::cross(e[2], norm),
                 };*/
 
-                /* for each vertex if its correspond edge is hindge */
+                /* for each vertex if its correspond edge is hindge
+                 * compute gradient of theta 
+                 */
                 for(int i = 0; i < 3; i++)
                 {
                     if (p->hindge_id[i] != -1)
                     {
-                        int h_id = p->hindge_id[i];
+                        int h_id = p->hindge_id[i] + offset;
 
                         int j = (i+1) % 3;
                         int k = (i+2) % 3;
 
+                        Vec3 tmp;
+
                         /* \dev_{x0} theta = - n / h0 */
-                        coefficients.push_back( T(h_id + offset, 3 * p->v_id[i]    , - norm.x/h[i]) );
-                        coefficients.push_back( T(h_id + offset, 3 * p->v_id[i] + 1, - norm.y/h[i]) );
-                        coefficients.push_back( T(h_id + offset, 3 * p->v_id[i] + 2, - norm.z/h[i]) );
+                        tmp = - norm * h_inv[i];
+                        theta_coeff.push_back( T(h_id, 3 * v_id[i]    , tmp.x) );
+                        theta_coeff.push_back( T(h_id, 3 * v_id[i] + 1, tmp.y) );
+                        theta_coeff.push_back( T(h_id, 3 * v_id[i] + 2, tmp.z) );
 
                         /* \dev_{x1} theta = n * cos2 / h1 */
-                        coefficients.push_back( T(h_id + offset, 3 * p->v_id[j]    , norm.x * cos[k]/h[j]) );
-                        coefficients.push_back( T(h_id + offset, 3 * p->v_id[j] + 1, norm.y * cos[k]/h[j]) );
-                        coefficients.push_back( T(h_id + offset, 3 * p->v_id[j] + 2, norm.z * cos[k]/h[j]) );
+                        tmp = norm * cos[k] * h_inv[j];
+                        theta_coeff.push_back( T(h_id, 3 * v_id[j]    , tmp.x) );
+                        theta_coeff.push_back( T(h_id, 3 * v_id[j] + 1, tmp.y) );
+                        theta_coeff.push_back( T(h_id, 3 * v_id[j] + 2, tmp.z) );
 
                         /* \dev_{x1} theta = n * cos1 / h2 */
-                        coefficients.push_back( T(h_id + offset, 3 * p->v_id[k]    , norm.x * cos[j]/h[k]) );
-                        coefficients.push_back( T(h_id + offset, 3 * p->v_id[k] + 1, norm.y * cos[j]/h[k]) );
-                        coefficients.push_back( T(h_id + offset, 3 * p->v_id[k] + 2, norm.z * cos[j]/h[k]) );
+                        tmp = norm * cos[j] * h_inv[k];
+                        theta_coeff.push_back( T(h_id, 3 * v_id[k]    , tmp.x) );
+                        theta_coeff.push_back( T(h_id, 3 * v_id[k] + 1, tmp.y) );
+                        theta_coeff.push_back( T(h_id, 3 * v_id[k] + 2, tmp.z) );
                     }
                 }
 
@@ -133,8 +136,23 @@ namespace BalloonFEM
             offset += f->hindges.size();
         }
 
+        /* compute bending force */
+        SpMat theta_grad(count_hindge, m_size);
+        theta_grad.setFromTriplets(theta_coeff.begin(), theta_coeff.end());
+
+        SpVec bendforce = dphi.transpose() * state.hindgeAngleGradient();
+
+        for(size_t i = 0; i < f_sum.size(); i++)
+        {
+            f_sum[i] += Vec3(
+                    bendforce(3*i    ), 
+                    bendforce(3*i + 1), 
+                    bendforce(3*i + 2));
+        }
 
         SpMat K(m_size, m_size);
+
+        K += theta_grad * ddphi * theta_grad.transpose();
         return K;
 
     }
